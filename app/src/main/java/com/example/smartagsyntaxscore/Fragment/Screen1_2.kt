a30c6848cd7c69f58227e47366fcd29cfcf7de6e
Şahin Karadeniz <sahinkaradeniiz@gmail.com>
2022-06-23 23:43:08 +0200

    screen transitions

--- app/src/main/java/com/example/smartagsyntaxscore/Fragment/Screen1_2.kt
+++ app/src/main/java/com/example/smartagsyntaxscore/Fragment/Screen1_2.kt
@@ -5,18 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.smartagsyntaxscore.R
+import com.example.smartagsyntaxscore.databinding.FragmentScreen12Binding
 
 
 class Screen1_2 : Fragment() {
 
+    private var _binding:FragmentScreen12Binding?=null
+    private val binding get() = _binding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_screen1_2, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+       _binding=FragmentScreen12Binding.inflate(inflater,container,false)
+        binding!!.nextButton13.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_screen1_2_to_screen1_3)
+        }
+        return binding?.root
     }
 
 
--- app/src/main/java/com/example/smartagsyntaxscore/Fragment/Screen1_3.kt
+++ app/src/main/java/com/example/smartagsyntaxscore/Fragment/Screen1_3.kt
@@ -7,19 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.example.smartagsyntaxscore.R
-import com.example.smartagsyntaxscore.databinding.FragmentScreen11Binding
+import com.example.smartagsyntaxscore.databinding.FragmentScreen13Binding
 
-class Screen1_1 : Fragment() {
-    private var _binding:FragmentScreen11Binding?=null
+
+class Screen1_3 : Fragment() {
+
+    private var _binding: FragmentScreen13Binding?=null
     private val binding get() = _binding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding=FragmentScreen11Binding.inflate(inflater,container,false)
+        _binding=FragmentScreen13Binding.inflate(inflater,container,false)
 
-        binding!!.nextButton12.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_screen1_1_to_screen1_2)
+        binding!!.nextButton14.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_screen1_3_to_screen1_4)
         }
 
-        return binding?.root
-    }
+        return binding!!.root
 
+    }
 }
