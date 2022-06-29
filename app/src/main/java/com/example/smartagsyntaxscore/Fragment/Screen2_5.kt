56588ad09f6e9728197df49bd4ed0fe57c6312fd
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2022-06-29 10:10:49 +0300

    screens

---
+++ app/src/main/java/com/example/smartagsyntaxscore/Fragment/Screen2_5.kt
@@ -0,0 +1,19 @@
+package com.example.smartagsyntaxscore.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.smartagsyntaxscore.R
+
+
+class Screen2_5 : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_screen2_5, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/smartagsyntaxscore/Fragment/Screen2_6.kt
@@ -0,0 +1,17 @@
+package com.example.smartagsyntaxscore.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.smartagsyntaxscore.R
+
+class Screen2_6 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_screen2_6, container, false)
+    }
+
+}
